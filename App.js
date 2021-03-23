5e00d49efc9374085b8c1bb4287d82a5b9f05ee8
Arunagirinathan Rajan <arunagirinathan.rajan@freshworks.com>
2021-03-23 14:41:35 +0530

    :tada: initial commit

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Header, Button, Text } from "react-native-elements";
 
 export default function App() {
+  const [value, setValue] = useState(0);
+  const increment = () => setValue((prev) => prev + 1);
+  const decrement = () => setValue((prev) => prev - 1);
+  const reset = () => setValue(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header
+        placement="left"
+        leftComponent={{
+          icon: "watch",
+          color: "#fff",
+          paddingLeft: 10,
+          paddingRight: 10,
+        }}
+        centerComponent={{
+          text: "Counter",
+          style: { color: "#fff", ...styles.text },
+        }}
+        containerStyle={styles.header}
+      />
+
+      <View>
+        <Text style={styles.value} h2>
+          {value}
+        </Text>
+        <View style={[styles.view]}>
+          <Button
+            buttonStyle={styles.buttonStyle}
+            style={styles.btn}
+            title="Increment"
+            onPress={increment}
+          />
+          <Button
+            buttonStyle={styles.buttonStyle}
+            style={styles.btn}
+            title="Decrement"
+            onPress={decrement}
+          />
+          <Button
+            buttonStyle={styles.buttonStyle}
+            style={styles.btn}
+            title="Reset"
+            onPress={reset}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +58,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  view: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  value: {
+    color: "black",
+    textAlign: "center",
+    paddingTop: 20,
+  },
+  text: {
+    fontSize: 23,
+  },
+  header: {
+    backgroundColor: "green",
+  },
+  btn: {
+    paddingTop: 10,
+  },
+  buttonStyle: {
+    width: 110,
+    backgroundColor: "green",
   },
 });
